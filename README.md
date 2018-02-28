cac21638f02a42df49d98e108cde32061e791513
xue <xjq9296@gmail.com>
2018-02-28 23:14:35 +0800

    init project

---
+++ README.md
@@ -0,0 +1,10 @@
+# AliGenie Calculator
+
+基于 AliGenie 开发者平台实现的智能语音计算器。
+
+# License
+
+[MIT](LICENSE.md)
+
+
+
---
+++ test/index.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const chai = require('chai')
+const assert = chai.assert
+const expect = chai.expect
+
+
+describe('HTTP GET', function() {
+    it('Get / should redirect to blog server', function() {
+        assert.equal(-1, -1);
+        expect(1).to.equal(1);
+    });
+});
+
+
+
+
